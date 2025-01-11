6d4c1ad40ff11acfbada62594f3b159339f280ee
felix mwangi <fmwauramwangi@gmail.com>
2025-01-12 00:26:18 +0300

    header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 function Header(props) {
-      return (
-        <h1>
-          {/* uncomment the line below to pass the test */}
-          {/* hello from the Header! */}
-        </h1>
-      )
+  return (
+    <>
+      <h1>hello from the Header!</h1>
+
+      {/* Uncomment the line below to pass the test */}
+      {/* <h1>hello from the Header!</h1> */}
+    </>
+  );
 }
 
 export default Header;
